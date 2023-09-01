95d727749807fc9ef972d0620b29e7f144593e12
cloud44 <zakair@DESKTOP-L9IALC9>
2023-08-31 18:55:00 -0600

    jss code file

---
+++ main.js
@@ -0,0 +1,14 @@
+/*I will make 6 constant var and then added them together and then take the results and then do a string concatenation in a alert box*/
+
+
+const input1 = 4; /*First number of the first formual*/
+const input2 = 6;/*Second number of the first formual*/
+const input3 = 18;/*First number of the second formual*/
+const input4 = 22;/*Second number of the second  formual*/
+const input5 = 15;/*First number of the third formual*/
+const input6 = 24;/*Second number of the third formual*/
+const dash = "-"; /*a dash to seperate the combo*/
+const combo1 = input1 + input2; /* added two const var to make a new const that is equal to 10*/
+const combo2 = input3 + input4;/* added two const var to make a new const that is equal to 40*/
+const combo3 = input5 + input6;/* added two const var to make a new const that is equal to 39*/
+window.alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:\n"  +  combo1 + dash + combo2 + dash + combo3); /*This will print the results of combo1,2,3 and then add it to the string in the alert box*/
